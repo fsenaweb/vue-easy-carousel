035d618375040e1e936c131e5d7d4b7b7b48895c
Matheus Ricelly <fsenaweb@gmail.com>
2020-08-20 14:40:49 -0300

    Corrections to component states and attributes

--- src/Carousel.vue
+++ src/Carousel.vue
@@ -2,18 +2,20 @@
   <div>
     <div class="container">
 
-      <div v-for="(image) in myImages" class="slides fade" :key="image">
-        <img :src="image[Object.keys(image)[0]]" :alt="image[Object.keys(image)[1]]"/>
-        <div v-if="showText" class="text">{{ image[Object.keys(image)[1]] }}</div>
+      <div v-for="(image, index) in myImages" ref="slides" class="slides fade" :key="index">
+        <img :src="image.image" :alt="image.text"/>
+        <div v-if="showText" class="text">{{ image.text }}</div>
       </div>
 
-      <a class="prev" @click.prevent="plusSlides(-1)">&#10094;</a>
-      <a class="next" @click.prevent="plusSlides(1)">&#10095;</a>
+      <a class="prev" @click.prevent="nextSlide(-1)">&#10094;</a>
+      <a class="next" @click.prevent="nextSlide(1)">&#10095;</a>
+
     </div>
 
     <div v-if="showDots" class="dots">
-      <span v-for="(_, index) in myImages" class="dot" :key="index" @click.prevent="currentSlide(index+ 1)"></span>
+      <span v-for="(_, index) in myImages" ref="dot" class="dot" :key="index" @click.prevent="currentSlide(index+ 1)"></span>
     </div>
+
   </div>
 </template>
 
@@ -22,7 +24,7 @@ export default {
   name: 'carousel',
   data() {
     return {
-      slideIndex: 1,
+      slideIndex: 1
     }
   },
   props: {
@@ -44,26 +46,28 @@ export default {
     this.showSlides(this.slideIndex)
   },
   methods: {
-    showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("slides");
-      var dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {
+    showSlides(x) {
+      let i;
+      let slides = this.$refs.slides;
+      if (x > slides.length) {
         this.slideIndex = 1
       }
-      if (n < 1) {
+      if (x < 1) {
         this.slideIndex = slides.length
       }
       for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
       }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
       slides[this.slideIndex - 1].style.display = "block";
-      dots[this.slideIndex - 1].className += " active";
+      if(this.showDots) {
+        let dots = this.$refs.dot;
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        dots[this.slideIndex - 1].className += " active";
+      }
     },
-    plusSlides(n) {
+    nextSlide(n) {
       this.showSlides(this.slideIndex += n);
     },
     currentSlide(n) {
@@ -78,14 +82,12 @@ export default {
   box-sizing: border-box
 }
 
-/* Slideshow container */
 .container {
   width: 100%;
   position: relative;
   margin: auto;
 }
 
-/* Hide the images by default */
 .slides {
   display: none;
 }
@@ -94,7 +96,6 @@ export default {
   width: 100%;
 }
 
-/* Next & previous buttons */
 .prev, .next {
   cursor: pointer;
   position: absolute;
@@ -110,40 +111,29 @@ export default {
   user-select: none;
 }
 
-/* Position the "next button" to the right */
 .next {
   right: 0;
   border-radius: 3px 0 0 3px;
 }
 
-/* On hover, add a black background color with a little bit see-through */
 .prev:hover, .next:hover {
   background-color: rgba(0, 0, 0, 0.8);
 }
 
-/* Caption text */
 .text {
   display: flex;
   justify-content: flex-start;
   align-items: center;
   color: #f2f2f2;
-  font-size: 15px;
+  font-size: 18px;
   padding: 8px 12px;
   position: absolute;
   bottom: 4px;
   width: 100%;
-  height: 50px;
+  height: 55px;
+  font-weight: bold;
   text-align: center;
   background-color: rgba(0, 0, 0, 0.4);
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
 }
 
 .dots {
@@ -151,7 +141,6 @@ export default {
   text-align: center;
 }
 
-/* The dots/bullets/indicators */
 .dot {
   cursor: pointer;
   height: 15px;
@@ -167,7 +156,7 @@ export default {
   background-color: #717171;
 }
 
-/* Fading animation */
+/* Fade animation */
 .fade {
   -webkit-animation-name: fade;
   -webkit-animation-duration: 1.5s;
