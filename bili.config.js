b1821f0d89ebd6a5f9445492a81e7afcf9986340
Matheus Ricelly <fsenaweb@gmail.com>
2026-02-12 17:02:27 -0300

    fix: Update build process for Node 20 compatibility (#33)
    - Updates bili.config.js to use the new API.
    - Simplifies the build script in package.json.
    - Downgrades rollup-plugin-vue to a Vue 2 compatible version.

--- bili.config.js
+++ bili.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   banner: true,
+  input: 'src/index.js',
   output: {
+    fileName: 'index[min][ext]',
+    format: ['cjs', 'umd', 'umd-min'],
+    moduleName: 'VueEasyCarousel',
     extractCSS: false,
   },
   plugins: {
